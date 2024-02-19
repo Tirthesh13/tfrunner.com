128a123a8f912cb0eec824060f1d491717fc8008
Samarjit Singh <samarjitsingh2000@gmail.com>
2024-02-19 20:38:50 +0530

    Added from Sam

--- Assets/Crowd Runner/Scripts/ChunkManager.cs
+++ Assets/Crowd Runner/Scripts/ChunkManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//from sam
 public class ChunkManager : MonoBehaviour
 {
     public static ChunkManager instance;
